eab0dd9d20524fb63616f4a3792b1ee07bc0e8bf
ali_ashour <aliashour592@gmail.com>
2022-10-13 15:22:02 +0200

    adding jquery library

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,6 @@
-    </body>
-</html>
+<?php
+    
+?>
+<script src="<?php echo $js ;?>jquery-3.6.1.min.js"></script>
+<script src="<?php echo $js ;?>jquery-ui.min.js"></script>
+<script src="<?php echo $js ;?>bootstrap.bundle.min.js"></script>
--- include/header.php
+++ include/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $css ; ?>reset.css">
+    <link rel="stylesheet" href="<?php echo $css ; ?>jquery-ui.min.css">
     <link rel="stylesheet" href="<?php echo $css ; ?>bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $css ; ?>fontawesome.css">
     <link rel="stylesheet" href="<?php echo $css ; ?>style.css">
