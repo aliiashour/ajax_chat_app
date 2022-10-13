35cbf6da5b07b85a7009def75c848eb02749926d
ali_ashour <aliashour592@gmail.com>
2022-10-13 15:23:28 +0200

    users status -online|offline- synchronization

--- connect_data_base.php
+++ connect_data_base.php
@@ -8,9 +8,20 @@
     try{
         $con    = new PDO($dsn, $user, $pass, $option);
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        function get_last_active_time($user_id, $co){
+            $q = "SELECT login_detail_user_last_activity FROM login_details WHERE login_detail_user_id = :user_id ORDER BY login_detail_user_last_activity DESC LIMIT 1" ;
+            $stmt = $co->prepare($q) ; 
+            $stmt->execute(array(
+                ':user_id' => $user_id
+            )) ; 
+            $row = $stmt->fetch() ; 
+            return $row['login_detail_user_last_activity'] ; 
+
+        }
     }
     catch (PDOException $e){
         echo $e ->getMessage() ; 
     }
+    
     
 ?>
